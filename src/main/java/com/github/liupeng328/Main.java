27275ef52427cf3cccc52ba211d39a706fd41037
刘鹏 <1029538990@qq.com>
2018-12-24 20:25:29 +0800

    gitignore

--- src/main/java/com/github/liupeng328/Main.java
+++ src/main/java/com/github/liupeng328/Main.java
@@ -1,14 +1,10 @@
 package com.github.liupeng328;
 
+import cn.hutool.db.Entity;
 import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpUtil;
-import cn.hutool.http.useragent.UserAgent;
 import cn.hutool.http.useragent.UserAgentUtil;
-import cn.hutool.json.JSON;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-
-import java.io.Serializable;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,13 +15,18 @@ public class Main {
     }
 
     /**
-     *下载视频
+     * 下载视频
      */
     private static void down() {
-       UserAgentUtil.parse(Const.userAgent);;
+        UserAgentUtil.parse(Const.userAgent);
+        ;
         String body = HttpRequest.get(Const.url).header("User-Agent", Const.userAgent).execute().body();
-        JSONObject jsonObject = JSONUtil.parseObj(body);
-        System.out.println(JSONUtil.formatJsonStr(body));
+        JSONObject json = JSONUtil.parseObj(body);
+        //System.out.println(JSONUtil.formatJsonStr(body));
+        //抽取数据 响应数据详细解析见： doc/抖音视频列表接口分析.json
+        //作者信息
+        Entity author = Entity.create()
+                .set()
 
     }
 }
